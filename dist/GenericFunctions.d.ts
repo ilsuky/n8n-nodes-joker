0eabb08b5abeac7eb094952679fe6e3556303a94
Christian <suky@tiroler-ac.at>
2022-10-04 13:06:26 +0200

    add cred2

--- dist/GenericFunctions.d.ts
+++
@@ -1,3 +0,0 @@
-import { IExecuteFunctions } from 'n8n-core';
-import { IDataObject, ILoadOptionsFunctions } from 'n8n-workflow';
-export declare function idoitRequest(this: IExecuteFunctions | ILoadOptionsFunctions, body?: IDataObject): Promise<any>;
